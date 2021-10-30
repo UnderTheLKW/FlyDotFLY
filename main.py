02df467059fbcdafcd78ffe1df5ddc274f83ed0e
UnderTheLKW <92048606+UnderTheLKW@users.noreply.github.com>
2021-10-30 19:06:40 +0200

    easteregg

--- main.py
+++ main.py
@@ -1,33 +1,75 @@
+EmptyObstacleY = 0
+ticks = 0
+obstacle: List[game.LedSprite] = []
+Bird: game.LedSprite = None
+rockets: List[game.LedSprite] = []
+Bird = game.create_sprite(0, 2)
+Bird.set(LedSpriteProperty.BLINK, 300)
+
+def launch_rocket():
+    global rockets
+    rocket = game.create_sprite(1, Bird.get(LedSpriteProperty.Y))
+    rockets.push(rocket)
+
 def on_button_pressed_a():
     Bird.change(LedSpriteProperty.Y, -1)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    Bird.change(LedSpriteProperty.Y, 1)
+    launch_rocket()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-EmptyObstacleY = 0
-Bird: game.LedSprite = None
-ticks = 0
-obstacle: List[game.LedSprite] = []
-Bird = game.create_sprite(0, 2)
-Bird.set(LedSpriteProperty.BLINK, 300)
+def check_gameover():
+    for obstacle3 in obstacle:
+        if obstacle3.get(LedSpriteProperty.X) == Bird.get(LedSpriteProperty.X) and obstacle3.get(LedSpriteProperty.Y) == Bird.get(LedSpriteProperty.Y):
+            game.game_over()
+
+def check_rocket_collision():
+    for rocket in rockets:
+        for obstacle3 in obstacle:
+            rocketx = rocket.get(LedSpriteProperty.X)
+            rockety = rocket.get(LedSpriteProperty.Y)
+            obstacleX = obstacle3.get(LedSpriteProperty.X)
+            obstacleY = obstacle3.get(LedSpriteProperty.Y)
+            if rocketx == obstacleX and rockety == obstacleY:
+                obstacle.remove_element(obstacle3)
+                obstacle3.delete()
+                rockets.remove_element(rocket)
+                rocket.delete()
 
 def on_forever():
     global EmptyObstacleY, ticks
     while len(obstacle) > 0 and obstacle[0].get(LedSpriteProperty.X) == 0:
         obstacle.remove_at(0).delete()
+
     for obstacle2 in obstacle:
         obstacle2.change(LedSpriteProperty.X, -1)
+
+    check_rocket_collision()
+
+    for rocket in rockets: 
+        rocket.change(LedSpriteProperty.X, 1)
+
+    check_rocket_collision()
+    check_gameover()
+
+    game.set_score(game.score() + 1)
+    
+    basic.pause(300 - ticks * 10)
+
+    Bird.change(LedSpriteProperty.Y, 1)
+
+    check_gameover()
+
     if ticks % 3 == 0:
         EmptyObstacleY = randint(0, 4)
         for index in range(5):
             if index != EmptyObstacleY:
                 obstacle.append(game.create_sprite(4, index))
-    for obstacle3 in obstacle:
-        if obstacle3.get(LedSpriteProperty.X) == Bird.get(LedSpriteProperty.X) and obstacle3.get(LedSpriteProperty.Y) == Bird.get(LedSpriteProperty.Y):
-            game.game_over()
-    ticks += 1
-    basic.pause(1000)
-basic.forever(on_forever)
 
+    ticks += 1
+
+    basic.pause(300 - ticks * 10)
+
+
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,15 +1,53 @@
+let EmptyObstacleY = 0
+let ticks = 0
+let obstacle : game.LedSprite[] = []
+let Bird : game.LedSprite = null
+let rockets : game.LedSprite[] = []
+Bird = game.createSprite(0, 2)
+Bird.set(LedSpriteProperty.Blink, 300)
+function launch_rocket() {
+    
+    let rocket = game.createSprite(1, Bird.get(LedSpriteProperty.Y))
+    rockets.push(rocket)
+}
+
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     Bird.change(LedSpriteProperty.Y, -1)
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    Bird.change(LedSpriteProperty.Y, 1)
+    launch_rocket()
 })
-let EmptyObstacleY = 0
-let Bird : game.LedSprite = null
-let ticks = 0
-let obstacle : game.LedSprite[] = []
-Bird = game.createSprite(0, 2)
-Bird.set(LedSpriteProperty.Blink, 300)
+function check_gameover() {
+    for (let obstacle3 of obstacle) {
+        if (obstacle3.get(LedSpriteProperty.X) == Bird.get(LedSpriteProperty.X) && obstacle3.get(LedSpriteProperty.Y) == Bird.get(LedSpriteProperty.Y)) {
+            game.gameOver()
+        }
+        
+    }
+}
+
+function check_rocket_collision() {
+    let rocketx: number;
+    let rockety: number;
+    let obstacleX: number;
+    let obstacleY: number;
+    for (let rocket of rockets) {
+        for (let obstacle3 of obstacle) {
+            rocketx = rocket.get(LedSpriteProperty.X)
+            rockety = rocket.get(LedSpriteProperty.Y)
+            obstacleX = obstacle3.get(LedSpriteProperty.X)
+            obstacleY = obstacle3.get(LedSpriteProperty.Y)
+            if (rocketx == obstacleX && rockety == obstacleY) {
+                obstacle.removeElement(obstacle3)
+                obstacle3.delete()
+                rockets.removeElement(rocket)
+                rocket.delete()
+            }
+            
+        }
+    }
+}
+
 basic.forever(function on_forever() {
     
     while (obstacle.length > 0 && obstacle[0].get(LedSpriteProperty.X) == 0) {
@@ -18,6 +56,16 @@ basic.forever(function on_forever() {
     for (let obstacle2 of obstacle) {
         obstacle2.change(LedSpriteProperty.X, -1)
     }
+    check_rocket_collision()
+    for (let rocket of rockets) {
+        rocket.change(LedSpriteProperty.X, 1)
+    }
+    check_rocket_collision()
+    check_gameover()
+    game.setScore(game.score() + 1)
+    basic.pause(300 - ticks * 10)
+    Bird.change(LedSpriteProperty.Y, 1)
+    check_gameover()
     if (ticks % 3 == 0) {
         EmptyObstacleY = randint(0, 4)
         for (let index = 0; index < 5; index++) {
@@ -28,12 +76,6 @@ basic.forever(function on_forever() {
         }
     }
     
-    for (let obstacle3 of obstacle) {
-        if (obstacle3.get(LedSpriteProperty.X) == Bird.get(LedSpriteProperty.X) && obstacle3.get(LedSpriteProperty.Y) == Bird.get(LedSpriteProperty.Y)) {
-            game.gameOver()
-        }
-        
-    }
     ticks += 1
-    basic.pause(1000)
+    basic.pause(300 - ticks * 10)
 })
